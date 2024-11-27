53d9758324f89a2146385f7b605e60db93c4f622
Gustav0Oliveira <gusoliveira2006@gmail.com>
2024-11-27 10:09:12 -0400

    crud corrigido

--- models/model_endereco.php
+++ models/model_endereco.php
@@ -1,6 +1,7 @@
 <?php 
-
-class Endereco {
+require_once "../config/database.php";
+require_once "crud.php";
+class Endereco implements Crud {
     private $conexao;
     private $tabela = 'endereco';
 
@@ -11,7 +12,7 @@ class Endereco {
     protected $numero;
     protected $bairro;
     protected $complemento;
--
+
 
     public function __construct($db){
         $this->conexao = $db;
@@ -24,37 +25,37 @@ class Endereco {
         return $resultado->fetch_all(MYSQLI_ASSOC);
     }
 
-    public function Criar(){
+    public function create(){
             $query = 'INSERT INTO {$this->tabela}(cidade, uf, rua, numero, bairro, complemento) VALUES ("' .$this->cidade . '", "' .$this->uf . '", "' .$this->rua . '", "' .$this->numero . '", "' .$this->bairro . '", "' .$this->complemento . '");';
             $resultado = $this->conexao->query($query);
             return $resultado;
     }
 
-    public function ler(){
+    public function read(){
             $query = "SELECT * FROM {$this->tabela} WHERE id_endereco = {$this->id_endereco}";
             $resultado = $this->conexao->query($query);
             return $resultado;
     }
 
-    public function Atualizar($atualizarEndereco){
+    public function update($valores){
         $query = 'UPDATE {$this->tabela} SET';
-        $colunasArray = array_keys($atualizarEndereco);
-            for($contador = 0; $contador < count($atualizarEndereco); $contador++){
+        $colunasArray = array_keys($valores);
+            for($contador = 0; $contador < count($valores); $contador++){
                 $coluna = $colunasArray[$contador];
-                $valor = $atualizarEndereco[$coluna];
+                $valor = $valores[$coluna];
 
-                $query .= $contador != (count($atualizarEndereco) - 1) ? $coluna .' = "'. $valor . '", ' : $coluna .' = "'. $valor .'" ';
+                $query .= $contador != (count($valores) - 1) ? $coluna .' = "'. $valor . '", ' : $coluna .' = "'. $valor .'" ';
             }
             $query += "WHERE id_endereco = {$this->id_endereco};";
             $resultado = $this->conexao->query($query);
             return $resultado;
     }
 
-    public function Excluir(){
+    public function delete(){
         $query =  "DELETE FROM {$this->tabela} WHERE id_endereco = {$this->id_endereco}";
         $resultado = $this->conexao->query($query);      
         return $resultado;
     }
 
-
+}
 ?>

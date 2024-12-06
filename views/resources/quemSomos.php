6c950bea0b851d14f0813c16ef056feeadc84d6e
higuinha123 <higafontourakaua@gmail.com>
2024-12-06 12:03:57 -0300

    tela de quem somos

--- views/resources/quemSomos.php
+++ views/resources/quemSomos.php
@@ -9,70 +9,77 @@
 <body>
     <?php include('../templates/navbarUser.php')?>
 
-    <section class="corpo container" >
+    <section class="corpo container">
         <div class="imagem_grupo">
             <img src="../images/grupo.png" alt="foto da equipe">
-        <h1>Nossa equipe</h1>
+            <div class="title-container">
+                <h1>Nossa equipe</h1>
+            </div>
+        </div>
 
         <div class="card-container">
-        <div class="card">
-            <img src="../images/emanuel.png" alt="Card 1">
-            <div class="card-content">
-                <div class="card-title">Emanuel Barros</div>
+            <div class="card">
+                <img src="../images/emanuel.png" alt="Card 1">
+                <div class="card-content">
+                    <div class="card-title">Emanuel Barros</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/lobortis.png" alt="Imagem do Card 2">
-            <div class="card-content">
-                <div class="card-title">lobortis Mattis</div>
+            <div class="card">
+                <img src="../images/lobortis.png" alt="Imagem do Card 2">
+                <div class="card-content">
+                    <div class="card-title">lobortis Mattis</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/marlene.png" alt="Imagem do Card 3">
-            <div class="card-content">
-                <div class="card-title">Marlene Pinho</div>
+            <div class="card">
+                <img src="../images/marlene.png" alt="Imagem do Card 3">
+                <div class="card-content">
+                    <div class="card-title">Marlene Pinho</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/niter.png" alt="Imagem do Card 4">
-            <div class="card-content">
-                <div class="card-title">Niter Salomão </div>
+            <div class="card">
+                <img src="../images/niter.png" alt="Imagem do Card 4">
+                <div class="card-content">
+                    <div class="card-title">Niter Salomão</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/emanuelle.png" alt="Imagem do Card 5">
-            <div class="card-content">
-                <div class="card-title">Emanuelle Nascimento</div>
+            <div class="card">
+                <img src="../images/emanuelle.png" alt="Imagem do Card 5">
+                <div class="card-content">
+                    <div class="card-title">Emanuelle Nascimento</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/sophia.png" alt="Imagem do Card 6">
-            <div class="card-content">
-                <h1>Sophia Silva</h1>
+            <div class="card">
+                <img src="../images/sophia.png" alt="Imagem do Card 6">
+                <div class="card-content">
+                   <div class="card-title"> Sophia Silva</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/anna.png" alt="Imagem do Card 7">
-            <div class="card-content">
-                <div class="card-title">Anna Coelho</div>
+            <div class="card">
+                <img src="../images/anna.png" alt="Imagem do Card 7">
+                <div class="card-content">
+                    <div class="card-title">Anna Coelho</div>
+                </div>
             </div>
-        </div>
 
-        <div class="card">
-            <img src="../images/gabriel.png" alt="Imagem do Card 8">
-            <div class="card-content">
-                <div class="card-title">Gabriel Coelho </div>
+            <div class="card">
+                <img src="../images/gabriel.png" alt="Imagem do Card 8">
+                <div class="card-content">
+                    <div class="card-title">Gabriel Coelho</div>
+                </div>
             </div>
         </div>
-    </div>
     </section>
 
     <?php include('../templates/footerUser.php')?>
+
+    <script>
+
+    </script>
 </body>
 </html>

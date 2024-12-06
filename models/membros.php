d01b72b4c12e0bcfa1a9f9b1c15a998456cf8822
higuinha123 <higafontourakaua@gmail.com>
2024-12-06 10:31:09 -0300

    model de membro

--- models/membros.php
+++ models/membros.php
@@ -3,7 +3,7 @@ class Membros {
     private $conexao;
     private $tabela = 'membro';
 
-    public $id_membro; 
+    public $id_membro;
     public $nome;
     public $email;
     public $senha;
@@ -12,35 +12,66 @@ class Membros {
     public function __construct($db){
         $this->conexao = $db;
     }
-
+  
     public function getIdMembro($id_membro){
-        $query = "SELECT * FROM {$this->tabela} WHERE id = {$this->id_membro}";
-        $resultado = $this->conexao->query($query);
+        $query = "SELECT * FROM {$this->tabela} WHERE id = ?";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bind_param("i", $id_membro);  
+        $stmt->execute();
+        $resultado = $stmt->get_result();
         return $resultado->fetch_all(MYSQLI_ASSOC);
     }
-    public function atualizar($atualizarMembro) {
-        $query = 'UPDATE membro SET';
-        $colunasArray = array_keys($atualizarMembro);
-        if($atualizarMembro){
-            for($contador = 0; $contador < count($); $contador++){
-                $coluna = $colunasArray[$contador];
-                $valor = $s[$coluna];
 
-                $query .= $contador != (count($valores) - 1) ? $coluna .' = "'. $valor .'", ' : $coluna .' = "'. $valor .'" ';
-            }
+    
+    public function uptade($atualizarMembro) {
+        if (empty($atualizarMembro)) {
+            throw new Exception('Dados para atualizar não fornecidos.');
+        }
 
-            return $query .= 'WHERE nome = "' . $this->nome . '";';
-        }else{
-            throw new Exception('Erro ao atualizar o membro:  '.$this->livro, 3);
+        $setClauses = [];
+        $params = [];
+        $types = '';
+
+        
+        foreach ($atualizarMembro as $coluna => $valor) {
+            $setClauses[] = "{$coluna} = ?";
+            $params[] = $valor;
+            $types .= is_int($valor) ? 'i' : 's';  
+        }
+
+        $query = "UPDATE {$this->tabela} SET " . implode(", ", $setClauses) . " WHERE id = ?";
+        $stmt = $this->conexao->prepare($query);
+
+       
+        $params[] = $this->id_membro;
+        $types .= 'i';                                                   // O ID é inteiro  0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-0-
+
+       
+        $stmt->bind_param($types, ...$params);
+
+        // consultaaaaa
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            throw new Exception('Erro ao atualizar o membro: ' . $stmt->error);
         }
     }
-    public function deletar($deletarMembro){
-        if($deletarMembro){
-            echo "DELETE FROM membro WHERE nome = '" . $this->nome . "';";
-        }else{
-            throw new Exception('Erro ao excluir o membro: '. $this->nome, 4);
+
+   
+    public function delete() {
+        if (empty($this->id_membro)) {
+            throw new Exception('ID do membro não fornecido.');
+        }
+
+        $query = "DELETE FROM {$this->tabela} WHERE id = ?";
+        $stmt = $this->conexao->prepare($query);
+        $stmt->bind_param("i", $this->id_membro);
+
+        if ($stmt->execute()) {
+            return true;
+        } else {
+            throw new Exception('Erro ao excluir o membro: ' . $stmt->error);
         }
     }
-
 }
 ?>

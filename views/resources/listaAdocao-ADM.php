e1dfb270ab3ce0103eab4e78131116b9fb6badb3
Gustav0Oliveira <gusoliveira2006@gmail.com>
2025-01-22 10:30:24 -0400

    Página completa

--- views/resources/listaAdocao-ADM.php
+++ views/resources/listaAdocao-ADM.php
@@ -1,54 +1,56 @@
 <!DOCTYPE html>
-<html lang="pt-br">
+<html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lista de Adoções - ADM</title>
-    <link rel="stylesheet" href="../css/listaAdocao-adm.css">
+    <title>Document</title>
+    <link rel="stylesheet" href="../css/listaAdocao-ADM.css">
 </head>
 <body>
     <?php include('../templates/navbarUser.php')?>
 
-    <div class="conteiner">
+    <section class="corpo container">
         <h1>Lista de Adoções</h1>
-
         <div class="pesquisar">
+            <img src="../images/lupa_pesguisa.png" alt="Lupa de pesquisa" class="lupa">
             <input type="text" placeholder="Pesquisar">
         </div>
-        
+
         <div class="lista-adocao">
             <div class="animal">
-                <img src="../images/Rectangle 71.png" alt="Rochele">
                 <h3>Rochele</h3>
+                <img src="../images/Rectangle 71.png" alt="Rochele">
                 <button>Excluir</button>
             </div>
             <div class="animal">
-                <img src="../images/Toby.png" alt="Toby">
                 <h3>Toby</h3>
+                <img src="../images/Toby.png" alt="Toby">
                 <button>Excluir</button>
             </div>
             <div class="animal">
-                <img src="../images/Rumi.png" alt="Rumi">
                 <h3>Rumi</h3>
+                <img src="../images/Rumi.png" alt="Rumi">
                 <button>Excluir</button>
             </div>
             <div class="animal">
-                <img src="../images/Nobre.png" alt="Nobre">
                 <h3>Nobre</h3>
+                <img src="../images/Nobre.png" alt="Nobre">
                 <button>Excluir</button>
             </div>
             <div class="animal">
-                <img src="../images/Sir Dougg.png" alt="Sir Dougg">
                 <h3>Sir Dougg</h3>
+                <img src="../images/Sir Dougg.png" alt="Sir Dougg">
                 <button>Excluir</button>
             </div>
             <div class="animal">
-                <img src="../images/Nigel.png" alt="Nigel">
                 <h3>Nigel</h3>
+                <img src="../images/Nigel.png" alt="Nigel">
                 <button>Excluir</button>
+            </div>
+            <div class="botao-add-container">
+                <button class="botao-add">+</button>
             </div>
         </div>
-
         <div class="altPag">
             <a href="#">&lt;</a>
             <a href="#" class="active">1</a>
@@ -56,9 +58,8 @@
             <a href="#">3</a>
             <a href="#">&gt;</a>
         </div>
-    </div>
 
-    <button class="botao-add">+</button>
+    </section>
 
     <?php include('../templates/footerUser.php')?>
 </body>

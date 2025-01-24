2c98cdb1ab3818da2bb37e8d013b06da61097e1a
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2025-01-24 12:04:11 -0300

    falta ajustar a logo e o ponto de arrecadação

--- views/resources/comoAjudar.php
+++ views/resources/comoAjudar.php
@@ -17,13 +17,13 @@
     <section class="pontoArrecadacao">
       <section class="infoDestaque">
         <div class="imgPontoArrecadacao">
-          <img src="../images/valinhos07 1.png" alt="Imagem da ONG Sos Patinhas" style="width: 100%; height:  auto; max-width: 450px;">
+          <img src="../images/valinhos07 1.png" alt="Imagem da ONG Sos Patinhas" >
+          <p class="endereco">R. do Parque, 75 - Centro, Campo Grande - MS</p>
         </div>
         <div class="descricaoPonto">
           <h2>Ponto de Arrecadação</h2>
           <p>"Seu gesto de doação é o elo que une corações humanos e patas amigas, criando um mundo mais compassivo para
             todos."</p>
-          <p>R. do Parque, 75 - Centro, Campo Grande - MS</p>
         </div>
       </section>
 
@@ -32,53 +32,64 @@
           <h2>Valores para Doação</h2>
             <div class="cards">
               <div class="card">
-                <img src="../images/iconeTalher.png" alt="Ícone de talher">
-                <h3>Kit Ração</h3>
-                <p>R$ 50</p>
+                <img src="../images/iconeTalher.png" alt="Ícone de talher" >
+                <div>
+                  <p><strong>Kit Ração</strong></p>
+                  <p>R$ 50</p>
+                </div>
               </div>
 
               <div class="card">
                 <img src="../images/iconeLimpeza.png" alt="Ícone de Limpeza">
-                <h3>Kit Limpeza</h3>
-                <p>R$ 25</p>
+                <div>
+                  <p><strong>Kit Limpeza</strong></p>
+                  <p>R$ 25</p>
+                </div>
               </div>
 
               <div class="card">
                 <img src="../images/iconeConforto.png" alt="Ícone de Conforto">
-                <h3>Kit Conforto</h3>
-                <p>R$ 50</p>
+                <div>
+                  <p><strong>Kit Conforto</strong></p>
+                  <p>R$ 50</p>
+                </div>
               </div>
 
               <div class="card">
                 <img src="../images/iconeMedico.png" alt="Ícone de Médico">
-                <h3>Kit Médico</h3>
-                <p>R$ 100</p>
+                <div>
+                  <p><strong>Kit Médico</strong></p>
+                  <p>R$ 100</p>
+                </div>
               </div>
 
               <div class="card">
                 <img src="../images/iconeAtendimento.png" alt="Ícone de Atendimento">
-                <h3>Kit Atendimento</h3>
-                <p>R$ 60</p>
+                <div>
+                  <p><strong>Kit Atendimento</strong></p>
+                  <p>R$ 60</p>
+                </div>
               </div>
 
               <div class="card">
                 <img src="../images/iconeVacina.png" alt="Ícone de Vacina">
-                <h3>Kit Vacinação</h3>
-                <p>R$ 40</p>
+                <div>
+                  <p><strong>Kit Vacinação</strong></p>
+                  <p>R$ 40</p>
+                </div>
               </div>
             </div>
           </div>
 
         <div class="qrcode">
           <h2>Pix QR Code ou CNPJ</h2>
-          <img src="../images/imagem_qrcode.png" alt="QR Code para Doação">
-          <p>ChaveCNPJ: XX. XXX. XXX/0001-XX</p>
-
-          <div class="dadosConta">
-            <h3>Sos Patinhas</h3>
-            <p>Conta: 198655-4</p>
-            <p>Agência: 1193</p>
-          </div>
+          <img src="../images/imagem_qrcode.png" alt="QR Code para Doação" class="imgQrCode">
+          <br></br>
+          <h3>Sos Patinhas</h3>
+          <h1>Conta: 198655-4
+            <img src="../images/banco.png" alt="Logo Banco" class="imgLogoBanco"></h1>
+            <h1>Agência: 1193</h1>
+            <p>ChaveCNPJ: XX. XXX. XXX/0001-XX</p>
         </div>
       </section>
     </section>

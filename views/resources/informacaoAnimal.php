c16cab0bc2f0bf470cbae5a4ada3866e93f49c16
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2025-01-27 12:07:25 -0300

    #155 - no começo ainda

--- views/resources/informacaoAnimal.php
+++ views/resources/informacaoAnimal.php
@@ -73,37 +73,86 @@
     <section class="corpo container" >
         <section class="conteudo">
             <section class="sliderFotos">
-                <div class="imgPrincipal">
+                <!-- <div class="imgPrincipal">
                     <div class="item"><img src="../images/slide1.png" alt=""></div>
                 </div>
                 <div class="containerMiniaturas">
-                    <?php
-                        require_once '../../config/database.php';
-                        $banco = new Banco();
 
-                        $idAnimal = $_GET['id'];
+                    <div class="item"><img src="../images/slide2.png" alt=""></div>
+                    <div class="item"><img src="../images/slide3.png" alt=""></div>
+                    <div class="item"><img src="../images/slide4.png" alt=""></div>
+                
+                </div> -->
 
-                        if($idAnimal != null){$query = 'SELECT caminho_foto FROM foto where id_animal = ' . $idAnimal;}
-                        else{}
+                <!-- Container for the image gallery -->
+                <div class="container">
 
-                        $resultado = $banco->query($query);
-                        $fotos = $banco->fetch_all($resultado);
-                        
-                        foreach ($fotos as $miniaturas){
-                            
-                            ?>
-                            <div class="item">
-                                <img src="" alt="">
-                            </div>
-                            <?php
-                        }
-
-                        $banco->fechar();
-                                 
-                    ?>
+                <!-- Full-width images with number text -->
+                <div class="mySlides">
+                    <div class="numbertext">1 / 6</div>
+                        <img src="img_woods_wide.jpg" style="width:100%">
+                    </div>
+                <div class="mySlides">
                     
-                    
+                <div class="numbertext">2 / 6</div>
+                    <img src="img_5terre_wide.jpg" style="width:100%">
                 </div>
+
+                <div class="mySlides">
+                <div class="numbertext">3 / 6</div>
+                    <img src="img_mountains_wide.jpg" style="width:100%">
+                </div>
+
+                <div class="mySlides">
+                <div class="numbertext">4 / 6</div>
+                    <img src="img_lights_wide.jpg" style="width:100%">
+                </div>
+
+                <div class="mySlides">
+                <div class="numbertext">5 / 6</div>
+                    <img src="img_nature_wide.jpg" style="width:100%">
+                </div>
+
+                <div class="mySlides">
+                <div class="numbertext">6 / 6</div>
+                    <img src="img_snow_wide.jpg" style="width:100%">
+                </div>
+
+                <!-- Next and previous buttons -->
+                <a class="prev" onclick="plusSlides(-1)">&#10094;</a>
+                <a class="next" onclick="plusSlides(1)">&#10095;</a>
+
+                <!-- Image text -->
+                <div class="caption-container">
+                <p id="caption"></p>
+                </div>
+
+                <!-- Thumbnail images -->
+                <div class="row">
+                <div class="column">
+                    <img class="demo cursor" src="img_woods.jpg" style="width:100%" onclick="currentSlide(1)" alt="The Woods">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="img_5terre.jpg" style="width:100%" onclick="currentSlide(2)" alt="Cinque Terre">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="img_mountains.jpg" style="width:100%" onclick="currentSlide(3)" alt="Mountains and fjords">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="img_lights.jpg" style="width:100%" onclick="currentSlide(4)" alt="Northern Lights">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="img_nature.jpg" style="width:100%" onclick="currentSlide(5)" alt="Nature and sunrise">
+                </div>
+                <div class="column">
+                    <img class="demo cursor" src="img_snow.jpg" style="width:100%" onclick="currentSlide(6)" alt="Snowy Mountains">
+                </div>
+                </div>
+                </div>
+
+
+
+
             </section>
 
             <section class="info">
@@ -116,9 +165,28 @@
                 </button>
             </section>
         </section>
-        
     </section>
 
     <?php include('../templates/footerUser.php')?>
 </body>
 </html>
+
+
+<!-- <?php
+    require_once '../../config/database.php';
+    $banco = new Banco();
+
+    $idAnimal = $_GET['id'];
+
+    if($idAnimal != null){
+        $query = 'SELECT caminho_foto FROM foto where id_animal = ' . $idAnimal;
+        $resultado = $banco->query($query);
+        $fotos = $banco->fetch_all($resultado);
+        
+        foreach ($fotos as $miniaturas) {
+            echo '<div class="item"><img src="' . $miniaturas['caminho_foto'] . '" alt=""></div>';
+        }
+    }
+    else{}
+    $banco->fechar();   
+?> -->

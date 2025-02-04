f933d34d7fbd2caf3bd4f492937329ab4930fe85
Gustav0Oliveira <gusoliveira2006@gmail.com>
2025-02-04 10:32:02 -0400

    Adicionei uma pasta com as imagens usadas na pagina. Pagina completa.

---
+++ views/resources/listaArrecadacoesADM.php
@@ -0,0 +1,120 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista Arrecadações ADM</title>
+    <link rel="stylesheet" href="../css/listaArrecadacaoADM.css">
+</head>
+<body>
+    <?php include_once "../templates/navbarAdm.php"; ?>
+
+    <section class="corpo container">
+        <h1>Lista de Arrecadações</h1>
+        <div class="pesquisar">
+            <img src="../images/lupa_pesguisa.png" alt="Lupa de pesquisa" class="lupa">
+            <input type="text" placeholder="Pesquisar">
+        </div>
+
+        <div class="lista-arrecadacoes" id="lista-arrecadacoes">
+            <div class="arrecadacao">
+                <h3>Freguês</h3>
+                <img src="../images/listaArrecadacoes-ADM/fregues.png" alt="Imagem do evento">
+                <div class="metas">
+                    <span><b>Meta: R$ 1.000,00.</b></span> 
+                </div>
+                <button>Excluir</button>
+            </div>
+            <div class="arrecadacao">
+                <h3>Nina</h3>
+                <img src="../images/listaArrecadacoes-ADM/nina.png" alt="Imagem do evento">
+                <div class="metas">
+                <span><b>Meta: R$ 2.500,00.</b></span> 
+                </div>
+                <button>Excluir</button>
+            </div>
+            <div class="arrecadacao">
+                <h3>Cascudo</h3>
+                <img src="../images/listaArrecadacoes-ADM/cascudo.png" alt="Imagem do evento">
+                <div class="metas">
+                    <span><b>Meta: R$ 2.500,00.</b></span>  
+                </div>
+                <button>Excluir</button>
+            </div>
+            <div class="arrecadacao">
+                <h3>Luke</h3>
+                <img src="../images/listaArrecadacoes-ADM/luke.png" alt="Imagem do evento">
+                <div class="metas">
+                    <span><b>Meta: R$ 870,00.</b></span>  
+                </div>
+                <button>Excluir</button>
+            </div>
+            <div class="arrecadacao">
+                <h3>Hulk</h3>
+                <img src="../images/listaArrecadacoes-ADM/hulk.png" alt="Imagem do evento">
+                <div class="metas">
+                    <span><b>Meta: R$ 1.200,00.</b></span>  
+                </div>
+                <button>Excluir</button>
+            </div>
+            <div class="arrecadacao">
+                <h3>Godinho</h3>
+                <img src="../images/listaArrecadacoes-ADM/godinho.png" alt="Imagem do evento">
+                <div class="metas">
+                    <span><b>Meta: R$ 720,00.</b></span>  
+                </div>
+                <button>Excluir</button>
+            </div>
+            <div class="botao-add-container">
+                <a href="adicionar-animal.php"><button class="botao-add">+</button></a>
+            </div>
+        </div>
+        <div class="cta-slide">
+            <button id="prev" class="pagination-btn">&lt;</button>
+            <div class="altPag" id="paginacao">
+                <li class="link active" value="1">1</li>
+                <li class="link" value="2">2</li>
+                <li class="link" value="3">3</li>
+            </div>
+            <button id="next" class="pagination-btn">&gt;</button>
+        </div>
+    </section>
+
+    <script>
+        let links = document.getElementsByClassName('link');
+        let valorAtual = 0;
+
+        function activeLink() {
+            for (let i = 0; i < links.length; i++) {
+                links[i].classList.remove('active');
+            }
+            links[valorAtual].classList.add('active');
+        }
+
+        for (let i = 0; i < links.length; i++) {
+            links[i].addEventListener('click', function() {
+                valorAtual = i;
+                activeLink();
+            });
+        }
+
+        document.getElementById('prev').addEventListener('click', function() {
+            if (valorAtual > 0) {
+                valorAtual--;
+                activeLink();
+            }
+        });
+
+        document.getElementById('next').addEventListener('click', function() {
+            if (valorAtual < links.length - 1) {
+                valorAtual++;
+                activeLink();
+            }
+        });
+    </script>
+
+
+
+    <?php include_once "../templates/footerAdm.php"; ?>
+</body>
+</html>

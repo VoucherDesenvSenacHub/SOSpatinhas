3f735c36147affb8bd517d6784f3b1fa174ab1e1
Pedro Lucas Correia Martin <pedrolucasrbsg@gmail.com>
2025-02-19 11:02:44 -0400

    Project Sos Patinhas - 19/09/2025 - 11:04 - pagina formAdoção 70%

--- views/resources/formAdocao.php
+++ views/resources/formAdocao.php
@@ -1,18 +1,16 @@
-//
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Formulário de Adoção</title>
-    <link rel="stylesheet" href="../css/arrecadacao.css" href="../css/formAdocao.css">
+    <link rel="stylesheet" href="../css/formAdocao.css">
 </head>
 <body>
     <?php include('../templates/navbarUser.php')?>
 
     <section class="corpo container">
-
+    
         <h1>FORMULÁRIO DE ADOÇÃO</h1>
 
         <p>Olá! Estamos muito felizes por você escolher adotar um animalzinho do SOS Patinhas 🙂</p>
@@ -22,24 +20,61 @@
         <p>Se você estiver interessado em participar deste processo, pedimos que responda às seguintes perguntas, que são parte do nosso protocolo de adoção. Embora o questionário seja extenso, nosso objetivo é minimizar as devoluções futuras, o que seria prejudicial para o animal adotado.
         Portanto, solicitamos que você responda todas as perguntas com cuidado e atenção. Analisaremos suas respostas minuciosamente e entraremos em contato assim que possível!</p>
 
-        <input type="text">
+        <form>
+            <input type="text" name="nome" placeholder="Nome Completo*" required>
+            <input type="date" name="data_nascimento" placeholder="Data de Nascimento*" required>
+            <input type="text" name="cpf" placeholder="CPF*" required>
+            <input type="text" name="rg" placeholder="RG*" required>
+            <input type="email" name="email" placeholder="E-mail*" required>
+            <input type="text" name="ddd" placeholder="DDD*" required>
+            <input type="text" name="telefone" placeholder="Telefone*" required>
+            <input type="text" name="cidade" placeholder="Cidade:" required>
+            <input type="text" name="uf" placeholder="UF*" required>
+            <input type="text" name="rua" placeholder="Rua:" required>
+            <input type="text" name="number" placeholder="Número:" required>
+            <input type="text" name="bairro" placeholder="Bairro:" required>
+            <input type="text" name="complemento" placeholder="Complemento:" required>
 
+            <label for="pergunta1"></label>
+            <input type="text" name="pergunta1" placeholder="Pergunta1:" required>
+
+            <label for="pergunta2"></label>
+            <input type="text" name="pergunta2" placeholder="Pergunta2:" required>
+
+            <label for="pergunta3"></label>
+            <input type="text" name="pergunta3" placeholder="Pergunta3:" required>
+
+            <label for="pergunta4"></label>
+            <input type="text" name="pergunta4" placeholder="Pergunta4:" required>
+
+            <label for="pergunta5"></label>
+            <input type="text" name="pergunta5" placeholder="Pergunta5:" required>
+
+            <label for="pergunta6"></label>
+            <input type="text" name="pergunta6" placeholder="Pergunta6:" required>
+        </form>
+         
         <h2>TERMO DE COMPROMISO</h2>
-
+        
         <p>O adotante se declara pelo presente, ciente das responsabilidades de ter um animal e se
-        compromete a zelar pela sua saúde e segurança, não permitindo que o mesmo saia
-        desacompanhado, alimentando-o, visitando o veterinário regularmente e sempre que
-        necessário. Está ciente ainda que esta adoção será monitorada, o que pode incluir visitas
-        periódicas, e em caso de maus-tratos poderá responder criminalmente com pena de 3
-        meses a 1 ano de detenção e multa, conforme Lei Federal de crimes ambientais
-        No.9605, art32 de 12/02/98;</p>
+            compromete a zelar pela sua saúde e segurança, não permitindo que o mesmo saia
+            desacompanhado, alimentando-o, visitando o veterinário regularmente e sempre que
+            necessário. Está ciente ainda que esta adoção será monitorada, o que pode incluir visitas
+            periódicas, e em caso de maus-tratos poderá responder criminalmente com pena de 3
+            meses a 1 ano de detenção e multa, conforme Lei Federal de crimes ambientais
+            No.9605, art32 de 12/02/98;</p>
+            
+            <p>O adotante é ciente que passará por um período de adaptação com o animal, se
+                comprometendo a proteger, alimentar, e educar sem sofrimento. Sob nenhuma hipótese
+                poderá maltratar o animal ou puni-lo de forma violenta com agressões físicas e NUNCA
+                abandoná-lo por nenhum motivo frívolo, como mudança de endereço, férias, doença,
+                velhice ou quaisquer motivos não citados. Abandono é crime. Lei municipal No.13131
+                de 18 de Maio de 2001.</p>
 
-        <p>O adotante é ciente que passará por um período de adaptação com o animal, se
-        comprometendo a proteger, alimentar, e educar sem sofrimento. Sob nenhuma hipótese
-        poderá maltratar o animal ou puni-lo de forma violenta com agressões físicas e NUNCA
-        abandoná-lo por nenhum motivo frívolo, como mudança de endereço, férias, doença,
-        velhice ou quaisquer motivos não citados. Abandono é crime. Lei municipal No.13131
-        de 18 de Maio de 2001.</p>
+                <input type="checkbox" name="termo" required>
+                <label for="termo">Compreendo e aceito os Termos de Compromisso</label>
+
+                <button type="submit" name="enviar">Enviar</button>
     </section>
 
     <?php include('../templates/footerUser.php')?>

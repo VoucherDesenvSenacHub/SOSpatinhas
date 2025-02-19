bc268c62a069d18a7fabdaaa666d900ba776ff75
EvertonSilva06 <evertondasilva670@gmail.com>
2025-02-18 22:39:54 -0400

    carousel events

--- views/resources/eventos.php
+++ views/resources/eventos.php
@@ -13,16 +13,16 @@
 
 <section class="container">
         <h1 class="texto">Arrecadações</h1>
-        <div class="conteiner swiper">
-            <div class="carousel">
-                    <ul id="imgAnimal" class="card-list swiper-wrapper">
-                        <li class="item">
+            <div class="wrapper">
+                    <i id="left">l</i>
+                    <ul id="imgAnimal" class="carousel">
+                        <li class="card">
                             <img src="../images/eventosImg/eventos2.png" class="animal" alt="">
                             <h3 class="texto-overlay">Ajude o Freguês</h3>
                             <div class="box-anim"></div>
                             <div class="box-saibamais"><h1>Saiba mais</h1></div>
                         </li>
-                        <li class="item">
+                        <li class="card">
                             <img src="../images/eventosImg/eventos6.png" class="animal" alt="">
                             <h3 class="texto-overlay">Ajude a Nina</h3>
                             <div class="box-anim">
@@ -30,18 +30,16 @@
                             </div>
                             <div class="box-saibamais"><h1>Saiba mais</h1></div>
                         </li>
-                        <li class="item">
+                        <li class="card">
                             <img src="../images/eventosImg/eventos3.png" class="animal" alt="">
                             <h3 class="texto-overlay">Ajude o Cascudo</h3>
                             <div class="box-anim"></div>
                             <div class="box-saibamais"><h2>Saiba mais</h2></div>
                         </li>
-                    </ul>  
-                    <div class="swiper-pagination"></div>
-                    <div class="swiper-button-prev"></div>
-                    <div class="swiper-button-next"></div>
+                    </ul>
+                    <i id="right">r</i>  
             </div>
-        </div>
+        
         <h1 class="texto">Eventos</h1>
         <div id="imgEventos">
             <div class="item" id="item-carousel">
@@ -84,40 +82,77 @@
 <?php
 @include('../templates/footerUser.php');
 ?>
-<script src="https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js"></script>
+
 <script>
-    new Swiper('.carousel', {
-  
-  loop: true,
-  spaceBetween:30,
-
-  // If we need pagination
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
-    dynamicBullets: true
-  },
-
-  // Navigation arrows
-  navigation: {
-    nextEl: '.swiper-button-next',
-    prevEl: '.swiper-button-prev',
-  },
-
-  breakpoints:{
-      0:{
-          slidesPerView: 1
-      },
-      768:{
-          slidesPerView: 2
-      },
-      1024:{
-          slidesPerView: 3
-      }
-  }
+    const carousel = document.querySelector(".carousel");
+const arrows = document.querySelectorAll(".wrapper i");
+const firstCard = carousel.querySelector(".card").offsetWidth;
+const carouselChildrens = [...carousel.children];
 
 
+/// carrosselll
+
+
+let cardPerView = Math.round(carousel.offsetWidth / firstCard);
+
+carouselChildrens.slice(-cardPerView).reverse().forEach(card => {
+    carousel.insertAdjacentHTML("afterbegin", card.outerHTML);
 });
+
+carouselChildrens.slice(0, cardPerView).forEach(card => {
+    carousel.insertAdjacentHTML("beforeend", card.outerHTML);
+});
+
+arrows.forEach(btn => {
+    btn.addEventListener("click", () =>{
+        carousel.scrollLeft += btn.id === "left" ? -firstCard : firstCard;
+    });
+});
+
+
+
+// scrolll
+let isDragging = false , startX , startScrollLeft;
+
+const dragStart = (e) =>{
+    isDragging = true;
+    carousel.classList.add("dragging");
+    startX = e.pageX;
+    startScrollLeft = carousel.scrollLeft;
+}
+
+
+const dragging = (e) => {
+    if(!isDragging) return;
+    carousel.scrollLeft = startScrollLeft - (e.pageX - startX);
+}
+
+const dragStop = () =>{
+    isDragging = false;
+    carousel.classList.remove("dragging");
+}
+
+const infiniteScroll = () =>{
+    if(carousel.scrollLeft === 0 ){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft = carousel.scrollWidth - (2 * carousel.offsetWidth );
+        carousel.classList.remove("no-transition");
+
+    } else if(Math.ceil(carousel.scrollLeft) === carousel.scrollWidth - carousel.offsetWidth){
+        carousel.classList.add("no-transition");
+        carousel.scrollLeft =  carousel.offsetWidth;
+        carousel.classList.remove("no-transition");
+
+    }
+}
+
+carousel.addEventListener("mousedown", dragStart);
+carousel.addEventListener("mousemove", dragging);
+document.addEventListener("mouseup", dragStop);
+carousel.addEventListener("scroll", infiniteScroll);
+
+
+
 </script>
 
 </body>

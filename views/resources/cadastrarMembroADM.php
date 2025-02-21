dca81e9912dfde355bedb19deb9fa131b21cf802
agathamarie <agatha06arantes@gmail.com>
2025-02-21 11:13:58 -0300

    #179 | Página finalizada e responsividade também

---
+++ views/resources/cadastrarMembroADM.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adicionar Membro</title>
+    <link rel="stylesheet" href="../css/cadastrarMembro.css">
+</head>
+<body>
+    <?php include('../templates/navbarAdm.php'); ?>
+
+    <section id="corpocontainer">
+
+        <div id="contentEditarPerfil">
+            <div id="divForm">
+                <form id="formEditarPerfilMembro">
+                    <input class="inputPerfil" type="text" placeholder="  Nome ">
+                    <input class="inputPerfil" type="text" placeholder="  E-mail  ">
+                    <input class="inputPerfil" type="text" placeholder="  Número Telefone/Celular  ">
+                    <input class="inputPerfil" type="text" placeholder="  Senha  ">
+                    <input class="inputPerfil" type="text" placeholder="  Confirmar Senha  ">
+                </form>
+            </div>
+            <div id="divButtons">
+                <div id="editarFoto">
+                    <img id="fotoMembro" src="../images/fotoPerfilMembro.png" alt="icone foto do Membro">
+                    <button id="buttonEditarFoto">+</button>
+                </div>
+
+                <a href="listademembrosadm.php"><button class="buttonsPerfil" id="buttonAdicionar">Adicionar</button></a>
+                <a href="listademembrosadm.php"><button class="buttonsPerfil" id="buttonCancelar">Cancelar</button></a>
+            </div>
+
+        </div>
+    </section>
+
+    <?php include('../templates/footerUser.php'); ?>   
+</body>
+</html>

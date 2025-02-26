bd93cae670906878035341a24e2b86ba81102666
agathamarie <agatha06arantes@gmail.com>
2025-02-26 09:51:50 -0300

    #156 finalizado

--- views/resources/formAdocao.php
+++ views/resources/formAdocao.php
@@ -21,37 +21,51 @@
         Portanto, solicitamos que você responda todas as perguntas com cuidado e atenção. Analisaremos suas respostas minuciosamente e entraremos em contato assim que possível!</p>
 
         <form>
-            <input type="text" name="nome" placeholder="Nome Completo*" required>
-            <input type="date" name="data_nascimento" placeholder="Data de Nascimento*" required>
-            <input type="text" name="cpf" placeholder="CPF*" required>
-            <input type="text" name="rg" placeholder="RG*" required>
-            <input type="email" name="email" placeholder="E-mail*" required>
-            <input type="text" name="ddd" placeholder="DDD*" required>
-            <input type="text" name="telefone" placeholder="Telefone*" required>
-            <input type="text" name="cidade" placeholder="Cidade:" required>
-            <input type="text" name="uf" placeholder="UF*" required>
-            <input type="text" name="rua" placeholder="Rua:" required>
-            <input type="text" name="number" placeholder="Número:" required>
-            <input type="text" name="bairro" placeholder="Bairro:" required>
-            <input type="text" name="complemento" placeholder="Complemento:" required>
+
+            <input type="text" name="nome" placeholder="Nome Completo*">
+
+            <div class="divMetade">
+                <input type="text" name="cpf" placeholder="CPF*">
+                <input type="text" name="rg" placeholder="RG*">
+            </div>
+            <div class="divMetade">
+                <input type="email" name="email" placeholder="E-mail*">
+                <input type="date" name="data_nascimento" placeholder="Data de Nascimento*">
+            </div>
+            <div class="divMetade">
+                <input id="inputddd" type="text" name="ddd" placeholder="DDD*">
+                <input type="text" name="telefone" placeholder="Telefone*">
+            </div>
+            <div class="divMetade">
+                <input type="text" name="cidade" placeholder="Cidade:">
+                <input id="inputuf" type="text" name="uf" placeholder="UF*">
+            </div>
+            <div class="divMetade">
+                <input type="text" name="rua" placeholder="Rua:">
+                <input id="inputnum" type="text" name="number" placeholder="Número:">
+            </div>
+            <div>
+                <input type="text" name="bairro" placeholder="Bairro:">
+                <input type="text" name="complemento" placeholder="Complemento:">
+            </div>
 
             <label for="pergunta1"></label>
-            <input type="text" name="pergunta1" placeholder="Pergunta1:" required>
+            <input type="text" name="pergunta1" placeholder="Pergunta1:">
 
             <label for="pergunta2"></label>
-            <input type="text" name="pergunta2" placeholder="Pergunta2:" required>
+            <input type="text" name="pergunta2" placeholder="Pergunta2:">
 
             <label for="pergunta3"></label>
-            <input type="text" name="pergunta3" placeholder="Pergunta3:" required>
+            <input type="text" name="pergunta3" placeholder="Pergunta3:">
 
             <label for="pergunta4"></label>
-            <input type="text" name="pergunta4" placeholder="Pergunta4:" required>
+            <input type="text" name="pergunta4" placeholder="Pergunta4:">
 
             <label for="pergunta5"></label>
-            <input type="text" name="pergunta5" placeholder="Pergunta5:" required>
+            <input type="text" name="pergunta5" placeholder="Pergunta5:">
 
             <label for="pergunta6"></label>
-            <input type="text" name="pergunta6" placeholder="Pergunta6:" required>
+            <input type="text" name="pergunta6" placeholder="Pergunta6:">
         </form>
          
         <h2>TERMO DE COMPROMISO</h2>
@@ -72,7 +86,7 @@
             de 18 de Maio de 2001.</p>
             
         <div class="checkbox">
-            <input type="checkbox" name="termo" required>
+            <input type="checkbox" name="termo">
             <label for="termo">Compreendo e aceito os Termos de Compromisso</label>
         </div>
 

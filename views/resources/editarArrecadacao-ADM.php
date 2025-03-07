f76a8d64d22e302df08b0774bad2658e717adda5
Yasmin-Leticia <yasminpinho199@gmail.com>
2025-03-07 08:38:26 -0300

    linkando css

--- views/resources/editarArrecadacao-ADM.php
+++ views/resources/editarArrecadacao-ADM.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Cadastro de Arrecadação</title>
-    <link rel="stylesheet" href="../css/editarArrecadacao.css">
+    <link rel="stylesheet" href="../css/editarArrecadacao-ADM.css">
 </head>
 <body>
     <?php include('../templates/navbarUser.php')?>

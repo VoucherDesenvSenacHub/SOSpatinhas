cac6c3c371b4b823f6eeefa48ac9b8d5628316ce
gustav0Oliveira <gusoliveira2006@gmail.com>
2025-03-13 08:26:24 -0300

    task 236 - 30%

---
+++ views/templates/componenteButton.php
@@ -0,0 +1,15 @@
+<button><a href="<?php $link ?>"><?php $tiulo ?></a></button>
+
+<style>
+    button {
+        font-family:  Arial, Helvetica, sans-serif;
+        width: 100%;
+        padding: 12px;
+        background-color: rgb( 68, 128, 63);
+        color: black;
+        border: none;
+        border-radius: 8px;
+        cursor: pointer;
+        font-size: 20px;
+    }
+</style>

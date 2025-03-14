0c2dfa2408b99b0eb57636fd3549acb88f9e412c
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2025-03-14 12:10:10 -0300

    #236 - 90% td pronto só testar

--- views/templates/componenteButton.php
+++ views/templates/componenteButton.php
@@ -1,15 +1,65 @@
-<button><a href="<?php echo $link ?>"><?php echo $titulo ?></a></button>
+<button 
+    onclick = "<?php echo $funcaoClick ?>" 
+    <?php if (!empty($funcaoLoad)) echo 'onload="'.$funcaoLoad.'"'; ?> 
+    id = "btnComp"
+    >
+<?php echo $titulo ?></button>
+
+<script>
+    function redirecionar(linkRedirecionamento){
+        window.location.href = linkRedirecionamento;
+    }
+
+    function compartilhar(){
+        if (navigator.share) {
+            navigator.share({ url: window.location.href }).catch((error) => console.log("Erro de Compartilhamento:", error));
+        } else {
+            alert("Erro de compartilhamento por conta do navegador.");
+        }
+    }
+
+    function adicionar(msgAdicionar){
+        if([...document.querySelectorAll("input")].every(input => input.value.trim() !== "")){
+            alert($msgAdicionar)
+        }else{
+            alert("Preencha todos os campos.")
+        }
+    }
+
+    function cancelarForm(nomeForm, linkRedirecionamento){
+        document.getElementById(nomeForm).reset();
+        window.location.href = linkRedirecionamento;
+    }
+
+    function verMais(){
+
+    }
+    
+    function excluir(msg){
+        let card = this.parentElement;
+        card.remove();
+        alert(msg);
+    }
+
+    function saibaMais(linkRedirecionamento){
+        window.location.href = linkRedirecionamento;
+        //muda o css 
+    }
+
+    function mudarTamanho(w, h){
+        const btn = document.getElementById('btnComp');
+        btnComp.style.width = w + 'rem';
+        btnComp.style.height = h + 'rem';
+    }
+</script>
 
 <style>
-    button {
-        font-family:  Arial, Helvetica, sans-serif;
-        width: 100%;
-        padding: 12px;
-        background-color: rgb( 68, 128, 63);
-        color: black;
-        border: none;
-        border-radius: 8px;
+    #btnComp {
+        border-radius: 12px;
+        background-color: #44803F;
         cursor: pointer;
-        font-size: 20px;
+        font-size: 24px;
+        color: #ffffff;
+        font-weight: 600;
     }
 </style>

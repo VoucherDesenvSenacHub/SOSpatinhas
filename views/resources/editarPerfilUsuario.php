0199c9b1fea7040d273f66e3c8c8c7c69fc49191
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2025-03-19 12:19:12 -0300

    #268 - Falta colocar os btns e terminar o css

---
+++ views/resources/editarPerfilUsuario.php
@@ -0,0 +1,56 @@
+<?php
+    $cssLink  = '../css/editarPerfilUsuario.css';
+    $tipo = 'User';
+    include('../templates/default/topHTML.php');
+?>
+
+<form action="" class="frmEditarPerfil">
+    <div class="conteudoFrm">
+        <div class="col">
+            <div class="upload-container">
+                <div id="filePreview">
+                    <img src="..\images\userProfile.png" alt="" id="previewImg">
+                </div>
+                <div id="imgUpload">
+                    <input type="file" id="image" name="image[]" accept="image/*" hidden>
+                    <label for="image" id="imgLabel" class="upload-box">
+                        <img src="..\images\btnAdicionar.png" alt="Upload Icon">
+                    </label>
+                </div>
+            </div>
+            
+            <div class="btnContainer">
+                <?php
+                    $funcaoClick = "";
+                ?>
+            </div>
+        </div>
+    
+    
+        <div class="col">
+            <input type="text" name="nome" placeholder="Nome" required>
+            <input type="date" name="dtNasc" placeholder="Data de Nascimento" required>
+            <input type="text" name="telefone" placeholder="Telefone" required>
+            <input type="email" name="email" placeholder="Email" required>
+            <input type="text" name="cpf" placeholder="CPF" required>
+            <input type="text" name="rg" placeholder="RG" required>
+        </div>
+    </div>
+</form>
+
+<script>
+    document.getElementById('image').addEventListener('change', function (event) {
+        const file = event.target.files[0]; 
+        if (file) {
+            const reader = new FileReader();
+            reader.onload = function (e) {
+                document.getElementById('previewImg').src = e.target.result; // Display the image
+            }
+            reader.readAsDataURL(file); // Convert file to base64
+        }
+    });
+</script>
+
+<?php
+    include('../templates/default/bottomHTML.php');
+?>

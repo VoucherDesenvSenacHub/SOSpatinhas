2fbe541a4fada80e42f7a39e9e625c2ed5fcfd98
Luan-Flores <keeganluan919@gmail.com>
2025-03-19 09:23:35 -0400

    #272 (REVISÃO - LER DESCRIÇÃO) Ajeitar a pag. Blog 19/03 09:20

--- views/resources/blog.php
+++ views/resources/blog.php
@@ -9,7 +9,7 @@
         <div class="grandin" id="cima">
         <a href="" class="link-not">
 
-            <img src="../images/Rectangle 272.png" alt="Article Image 1"  class="img-first">
+            <img src="" alt="Article Image 1"  class="img-first" style='visibility: hidden'>
             <div class="titulo">
                 <h2>Título da notícia</h2>
                 <p class="p-none">Display none</p>
@@ -19,7 +19,7 @@
     <div class="grandin" id="baixo">
         <a href="" class="link-not">
             
-            <img src="../images/Rectangle 273.png" alt="Article Image 2"  class="img-first">
+            <img src="" alt="Article Image 2"  class="img-first" style='visibility: hidden'>
             <div class="titulo">
                 <h2>Título da notícia</h2>
                 <p class="p-none">Display none</p>
@@ -27,18 +27,19 @@
         </a>    
         </div>
     </div>
+    
     <style>
         .p-none{
             display: none;
         }
     </style>
-    
+
     <aside class="sidebarBlog">
         
         <div class="ladin" id="pit">
         <a href="" class="link-not">
 
-            <img src="../images/Rectangle 274.png" alt="Sidebar Image 1" class="img-ladin">
+            <img src="../images/Rectangle 274.png" alt="Sidebar Image 1" class="img-ladin" style='visibility: hidden'>
             <div class="titulo">
                 <h2>Título da notícia</h2>
                 <p>Não vai ser fácil esquecer Não vai ser fácil esquecer Não vai ser fácil esquecer Não vai ser fácil esquecer</p>
@@ -48,7 +49,7 @@
         <div class="ladin" id="maus_tratos">
         <a href="" class="link-not">
 
-            <img src="../images/Rectangle 275.png" alt="Sidebar Image 2" class="img-ladin">
+            <img src="../images/Rectangle 275.png" alt="Sidebar Image 2" class="img-ladin" style='visibility: hidden'>
             <div class="titulo">
                 <h2>Título da notícia</h2>
                 <p>Não vai ser fácil esquecer Não vai ser fácil esquecer Não vai ser fácil esquecer Não vai ser fácil esquecer</p>
@@ -58,7 +59,7 @@
         <div class="ladin" id="tatu">
         <a href="" class="link-not">
 
-            <img src="../images/Rectangle 276.png" alt="Sidebar Image 3" class="img-ladin">
+            <img src="../images/Rectangle 276.png" alt="Sidebar Image 3" class="img-ladin" style='visibility: hidden'>
             <div class="titulo">
                 <h2>Título da notícia</h2>
                 <p>Não vai ser fácil esquecer Não vai ser fácil esquecer Não vai ser fácil esquecer Não vai ser fácil esquecer</p>
@@ -72,7 +73,7 @@
     <div class="grandin" id="cima">
     <a href="" class="link-not">
 
-        <img src="../images/Rectangle 272.png" alt="Article Image 1" class="img-ladin">
+        <img src="../images/Rectangle 272.png" alt="Article Image 1" class="img-ladin" style='visibility: hidden'>
         <div class="titulo">
             <h2>Título da notícia</h2>
             <p>sahudihausd</p>
@@ -82,7 +83,7 @@
     <div class="grandin" id="baixo">
     <a href="" class="link-not">
 
-        <img src="../images/Rectangle 273.png" alt="Article Image 2" class="img-ladin">
+        <img src="../images/Rectangle 273.png" alt="Article Image 2" class="img-ladin" style='visibility: hidden'>
         <div class="titulo">
             <h2>Título da notícia</h2>
             <p>asuifhusead</p>
@@ -122,8 +123,9 @@
     </div>
 </section>
 
-
-<script src="../script/blog.js"></script>
+<script src="../script/blog.js">
+  window.addEventListener("load", obterNoticia());
+</script>
 
 <?php
     include('../templates/default/bottomHTML.php');
--- views/script/blog.js
+++ views/script/blog.js
@@ -1,76 +1,87 @@
-async function obterNoticia() {
-    
-    try{
 
-        // const url = `https://newsapi.org/v2/everything?q=${endpoint}&apiKey=31480fb9ae244876aac77345786bc5ec`;
-        const url = `https://newsapi.org/v2/everything?q=(gato OR cachorro) AND (engraçado OR adorável OR filhote)&language=pt&apiKey=31480fb9ae244876aac77345786bc5ec`;
-
-        const titulo = document.getElementsByTagName('h2') 
-        const imgGrande = document.querySelectorAll('.img-first')
-        const imgLado = document.querySelectorAll('.img-ladin')
-        const subTitulo = document.getElementsByTagName('p')
-        console.log(imgGrande)
-        for (var i = 0; i < 5; i++){
-            console.log("1")
-        }
+    async function obterNoticia() {
         
-        console.log(titulo)
-        const response = await fetch(url);
-        // console.log(response)
-
-        const data = await response.json();
-        console.log(data)
-        const dataEncurt = data.articles.slice(0, 5)
-        console.log(dataEncurt)
-        // for (let urlImgNot of data.articles[0].src){
-        //     console.log(urlImgNot)
-        // }
-        let noticia = dataEncurt;
-        let imagens = [];
-        let titulos = [];
-        let subTitulos = [];
-
-        // Armazena URLs das imagens e títulos
-        noticia.forEach(not => {
-            let urlJSON = not.urlToImage;
-            let titleJSON = not.title;
-            let subTitleJSON = not.description;
-
-            imagens.push(urlJSON);
-            titulos.push(titleJSON);
-            subTitulos.push(subTitleJSON);
-        });
-
-        // Junta todos os elementos <img> desejados
-        const todasImagens = [...imgGrande, ...imgLado]; 
-        const todosTitulos = [...titulo]; // Converte `titulo` para array se necessário
-        const todosSubTitulos = [...subTitulo]; // Converte `titulo` para array se necessário
-
+        try{
+            
+            // const url = `https://newsapi.org/v2/everything?q=${endpoint}&apiKey=31480fb9ae244876aac77345786bc5ec`;
+            const url = `https://newsapi.org/v2/everything?q=(gato OR cachorro) AND (engraçado OR adorável OR filhote)&language=pt&apiKey=31480fb9ae244876aac77345786bc5ec`;
+            
+            const titulo = document.getElementsByTagName('h2') 
+            const imgGrande = document.querySelectorAll('.img-first')
+            const imgLado = document.querySelectorAll('.img-ladin')
+            const subTitulo = document.getElementsByTagName('p')
+            const aHref = document.querySelectorAll('.link-not')
+            
+            
+            console.log(imgGrande)
+            for (var i = 0; i < 5; i++){
+                console.log("1")
+            }
+            
+            console.log(titulo)
+            const response = await fetch(url);
+            
+            
+            const data = await response.json();
+            console.log(data)
+            const dataEncurt = data.articles.slice(0, 5)
+            console.log(dataEncurt)
+            
+            let noticia = dataEncurt;
+            let imagens = [];
+            let titulos = [];
+            let subTitulos = [];
+            let links = [];
+            
+            // Armazenamento das infos do JSON
+            noticia.forEach(not => {
+                let urlJSON = not.urlToImage;
+                let titleJSON = not.title;
+                let subTitleJSON = not.description;
+                let linkNotJSON = not.url;
+                
+                imagens.push(urlJSON);
+                titulos.push(titleJSON);
+                subTitulos.push(subTitleJSON);
+                links.push(linkNotJSON);
+            });
+            
+            // Junta todos os elementos <img> desejados
+            const todasImagens = [...imgGrande, ...imgLado]; 
+            const todosTitulos = [...titulo]; // Converte `titulo` para array se necessário
+            const todosSubTitulos = [...subTitulo]; // Converte `subTitulo` para array se necessário
+            const todosAHref = [...aHref];
+            
         // Atualiza imagens
         todasImagens.forEach((imgDiv, index) => {
             if (imagens[index]) {
                 imgDiv.src = imagens[index];
             }
         });
-
+        
         // Atualiza títulos
         todosTitulos.forEach((titleDiv, index) => {
             if (titulos[index]) {
                 titleDiv.innerText = titulos[index];
             }
         });
-
+        
         todosSubTitulos.forEach((subTitleDiv, index) => {
             if (subTitulos[index]) {
                 subTitleDiv.innerText = subTitulos[index];
+            }
+        });
+        todosAHref.forEach((aHrefc, index) => {
+            if (links[index]) {
+                aHrefc.href = links[index];
             }
         });
         for (sub of subTitulo){
             console.log(sub);
             limitarTexto(sub, 23); // Limite de palavras
         }
-
-
+        
+        mostrar()
     }
     catch (e) {
         console.log("Problem " + e)
@@ -79,13 +90,19 @@ async function obterNoticia() {
 function limitarTexto(elemento, limite) {
     let palavras = elemento.innerText.split(" ");
     if (palavras.length > limite) {
-      elemento.innerText = palavras.slice(0, limite).join(" ") + "...";
+        elemento.innerText = palavras.slice(0, limite).join(" ") + "...";
     }
-  }
-obterNoticia()
+}
+function mostrar(){
+    
+    document.querySelectorAll('img').forEach(img => {
+        img.style.visibility = "visible";
+        console.log(img + " <- IMAGEM ")
+    });
+    document.querySelectorAll('.titulo').forEach(divTitulo => {
+        divTitulo.style.visibility = "visible";
+        console.log(divTitulo + " <- DIVTITULO")
+    })
+}
 
-// let paragrafo = document.getElementsByTagName('p');
-// for (sub of paragrafo){
-//     console.log(sub);
-//     limitarTexto(sub, 10); // Limite de 10 palavras
-// }
+obterNoticia()

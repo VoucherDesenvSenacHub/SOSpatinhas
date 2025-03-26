d152d3ef2f4976c106c7f8eea177d5afc122a52d
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2025-03-26 12:15:00 -0300

    #notask - 50% responsividade da pág detalhes de evento

--- views/resources/detalhesEvento.php
+++ views/resources/detalhesEvento.php
@@ -4,37 +4,34 @@
   include('../templates/default/topHTML.php');
 ?>
 
-<div class="conteudo">
-
-  <div class="slideshow-container">
+<section class="conteudo">
+  <div class="imgEDetalhesEvento">
     <div class="slider">
-
       <div class="slide fade">
-        <img class= "EventoSlides" src="../images/horto-florestal.png" alt="Horto Florestal 1">
+        <img class= "eventoSlides" src="../images/horto-florestal.png" alt="Horto Florestal 1">
       </div>
 
       <div class="slide fade">
-        <img class= "EventoSlides" src="../images/eventosImg/feira.jpeg" alt="Horto Florestal 2">
+        <img class= "eventoSlides" src="../images/eventosImg/feira.jpeg" alt="Horto Florestal 2">
       </div>
 
       <div class="slide fade">
-        <img class= "EventoSlides" src="../images/eventosImg/feira1.jpg" alt="Horto Florestal 3">
+        <img class= "eventoSlides" src="../images/eventosImg/feira1.jpg" alt="Horto Florestal 3">
       </div>
 
-      <div class="btn-slide">
+      <div class="btnSlide">
         <span class="botao" onclick="OutroSlide(1)" > </span>
         <span class="botao" onclick="OutroSlide(2)" > </span>
         <span class="botao" onclick="OutroSlide(3)" > </span>
       </div>
 
       <div class="localEvento">
-        <img src="../images/marcador_1.png" alt="marcador_1">
+        <img src="../images/marcador_1.png" alt="Icone de local">
         <p>Campo Grande MS, Horto Florestal</p>
       </div>
-
     </div>
 
-    <div class="event-details">
+    <div class="detalhesEvento">
       <h1>Feira do Auau</h1>
       <p>Feira arrecadativa de moda no Horto Florestal, venha comprar roupas para você ou para seu pet! Todos os lucros da feira serão direcionados ao SOS Patinhas.</p>
       <?php
@@ -44,15 +41,13 @@
         include('../templates/componenteButton.php');
       ?>
     </div>
-    
   </div>
     
-  <div class="carrossel-container">
+  <div class="carrosselContainer">
     <h1 class="titulo">Outros Eventos</h1>
     <div id="eventosCarrosel">
       <?php
       $cardComponents2 = array();
-
       for ($i = 0; $i < 5; $i++) {
           ob_start(); 
           include('../templates/eventosCard.php');
@@ -61,21 +56,21 @@
 
       $cardComponents = $cardComponents2;
       $carouselId = 'carousel2';
-      @include('../templates/carossel.php');
+      include('../templates/carossel.php');
       ?>
     </div>
   </div>
-</div>
+</section>
 
 <script> 
   let PrimeiroSlide = 1;
   mostrarSlide (PrimeiroSlide);
 
-  function OutroSlide(n){
+  function OutroSlide(n) {
     mostrarSlide(PrimeiroSlide = n);
   }
 
-  function mostrarSlide(n){
+  function mostrarSlide(n) {
     let i;
     let slides = document.getElementsByClassName("slide");
     let dots =  document.getElementsByClassName("botao");
@@ -90,12 +85,6 @@
     slides[PrimeiroSlide-1].style.display = "block";
     dots[PrimeiroSlide-1].className += " active";
   }
-
-
-  // outros eventos
-
 </script>
 
-<?php
-    include('../templates/default/bottomHTML.php');
-?>
+<?php include('../templates/default/bottomHTML.php'); ?>

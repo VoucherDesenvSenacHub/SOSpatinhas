8428f21f3af795357f6f3e37bbc9990433ec3bea
iz4bella <izabellac161@gmail.com>
2025-04-02 12:12:13 -0300

    #299 - Cleanup, HotFix / limpei as linhas de codigo desnecessarias e alterei os direcionamentos, task ainda não finalizada.

--- views/adm/cadastrarMembro.php
+++ views/adm/cadastrarMembro.php
@@ -1,35 +1,27 @@
 <?php
 
-    // if (!isset($_SESSION['taLogado']) || $_SESSION['taLogado'] !== true || $_SESSION['id'] !== 'Admin') {
-    // echo '<script type="text/javascript">
-    //         alert("Você precisa estar logado como administrador para acessar esta página.");
-    //         window.location.href = "loginADM.php";
-    //       </script>';
-    // exit();
-    // }
-
-    $cssLink  = '../css/cadastrarMembroADM.css';
+    $cssLink  = '../../public/css/cadastrarMembroADM.css';
     $tipo = 'Adm';
-    include('../../../componentes/default/topHTML.php');
+    include('../../public/componentes/default/topHTML.php');
 ?>
 
 <form action="" method="post" id="cadastrarMembro">      
     <div id="conteudoForm">
-        <div id="col">
+        <div id="colunaForm">
         <div class="upload-container">
             <div id="filePreview">
-                <img src="..\images\userProfile.png" alt="" id="previewImg">
+                <img src="../../public/images/userProfile.png" alt="" id="previewImg">
             </div>
             <div id="imgUpload">
                 <input type="file" id="image" name="image[]" accept="image/*" hidden>
                 <label for="image" id="imgLabel" class="upload-box">
-                    <img src="..\images\btnAdicionar.png" alt="Upload Icon">
+                    <img src="../../public/images/btnAdicionar.png" alt="Upload Icon">
                 </label>
             </div>
         </div>   
     </div>
 
-        <form id="col">
+        <form id="colunaForm">
             <input required class="inputPerfil" type="text" placeholder="  Nome ">
             <input required class="inputPerfil" type="text" placeholder="  E-mail  ">
             <input required class="inputPerfil" type="text" placeholder="  Telefone">
@@ -42,13 +34,13 @@
         $funcaoClick = "adicionarComValidacao('Membro cadastrado com sucesso!')";
         $funcaoLoad = "mudarTamanho('btnCadastrar', '275px', '50px', '20px')";
         $titulo = "Cadastrar";
-        include('../templates/componenteButton.php');
+        include('../../public/componentes/componenteButton.php');
         
         $idBtn = "btnCancelar";
         $funcaoClick = "cancelarForm('frmCadastroAdocao','listaArrecadacoesADM.php')";
         $funcaoLoad = "mudarTamanho('btnCancelar', '275px', '50px', '20px')";
         $titulo = "Cancelar";
-        include('../templates/componenteButton.php');
+        include('../../public/componentes//componenteButton.php');
         ?>
         </form>
     </div>
@@ -67,5 +59,5 @@
 </script>
     
 <?php
-    include('../templates/default/bottomHTML.php');
+    include('../../public/componentes/default/bottomHTML.php');
 ?>

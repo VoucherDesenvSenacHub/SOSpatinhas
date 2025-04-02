281d11e9763a96f0dabbfd18b069905e1ea00477
Luan-Flores <keeganluan919@gmail.com>
2025-04-02 09:18:56 -0400

    #304 - Fix / Corrigi os links

--- views/user/confirmacaoEmail.php
+++ views/user/confirmacaoEmail.php
@@ -5,11 +5,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>SOS Patinhas</title>
-    <link rel="stylesheet" href="confirmacaoEmail.css">
+    <link rel="stylesheet" href="../../public/css/confirmacaoEmail.css">
+    <link rel="stylesheet" href="../../public/css/default.css">
 </head>
 
 <body>
-    <?php include('../templates/navbarUser.php')?>
+    <?php include('../../public/componentes/navbar.php')?>
     
     <section class="corpo-container">
 
@@ -17,12 +18,12 @@
             <div class="bloco">
                 <h2>E-mail enviado!</h2>
                 <p>Verifique sua caixa de entrada e clique no link para alterar a senha!</p>
-                <img src="../images/animais.png" alt="Aniamis">
+                <img src="../../public/images/animais.png" alt="Aniamis">
             </div>
         </section>
 
     </section>
         
-    <?php include('../templates/footerUser.php')?>
+    <?php include('../../public/componentes/footer.php')?>
 </body>
 </html>

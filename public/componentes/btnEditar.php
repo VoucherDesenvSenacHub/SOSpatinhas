a4538e1138e9340c472b4e3d5afd54f9e8b41f1a
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2025-04-10 15:54:08 -0400

    #notask - Feat / coloquei validação para mostrar o btn se o usuário for adm

--- public/componentes/btnEditar.php
+++ public/componentes/btnEditar.php
@@ -1,102 +1,104 @@
-<img src="../public/images/btnEditar.png" alt="" class="btnEditar">
-
-<script>
-const btnEditar = document.querySelector('.btnEditar');
-btnEditar.addEventListener('click', function()
-{
-  const divPai = this.parentNode;
-  const idDivPai = divPai.id;
-
-  const inputSimples = divPai.querySelector('h2');
-  const textarea = divPai.querySelector('p');
-  const imagem = divPai.querySelector('img:nth-of-type(2)');
-  abreModalEditar(inputSimples, textarea, imagem, idDivPai);
-            
-});
-
-function abreModalEditar(input, textarea, img, idPai) {
-  let conteudoModal = `
-    <div class="overlayEditar"></div>
+<?php if($isAdm = isset($_SESSION['tipo']) && $_SESSION['tipo'] === "Admin";): ?>
+  <img src="../public/images/btnEditar.png" alt="" class="btnEditar">
+  
+  <script>
+  const btnEditar = document.querySelector('.btnEditar');
+  btnEditar.addEventListener('click', function()
+  {
+    const divPai = this.parentNode;
+    const idDivPai = divPai.id;
+  
+    const inputSimples = divPai.querySelector('h2');
+    const textarea = divPai.querySelector('p');
+    const imagem = divPai.querySelector('img:nth-of-type(2)');
+    abreModalEditar(inputSimples, textarea, imagem, idDivPai);
+              
+  });
+  
+  function abreModalEditar(input, textarea, img, idPai) {
+    let conteudoModal = `
+      <div class="overlayEditar"></div>
+      
+      <div class="popupEditar">
+          <link rel="stylesheet" href="../../public/css/modalEditar.css">
+          <button id="btnFecharPopup" aria-label="fechar">X</button>
+          ;
     
-    <div class="popupEditar">
-        <link rel="stylesheet" href="../../public/css/modalEditar.css">
-        <button id="btnFecharPopup" aria-label="fechar">X</button>
-        ;
+    if (input) {
+        conteudoModal += `
+          <input type="text" id="inputBoxPopup" value="${input.textContent}">
+        `;
+    }
+    
+    if (textarea) {
+        conteudoModal += `
+          <textarea id="popup-textoArea" name="popup-textoArea">${textarea.textContent}</textarea>
+        `;
+    }
+    
+    if (img) {
+        conteudoModal += `
+          <div id="input_img">
+              <img src="../public/images/cadastroAdocao-ADM/grampoBranco.png" id="grampo">
+              <p id="enviar_foto">Enviar foto</p>
+              <input type="file" id="inputFilePopup" accept="image/*">
+              <img src="${img.src}" id="currentImgPreview" style="max-width: 100px;">
+          </div>
+        `;
+    }
+    
+    conteudoModal += `
+      <button id="submitForm" onclick="submitForm(${input.textContent},${textarea.value},${img.src},${idPai})">Salvar</button>
+    </div>`;
   
-  if (input) {
-      conteudoModal += `
-        <input type="text" id="inputBoxPopup" value="${input.textContent}">
-      `;
-  }
-  
-  if (textarea) {
-      conteudoModal += `
-        <textarea id="popup-textoArea" name="popup-textoArea">${textarea.textContent}</textarea>
-      `;
-  }
-  
-  if (img) {
-      conteudoModal += `
-        <div id="input_img">
-            <img src="../public/images/cadastroAdocao-ADM/grampoBranco.png" id="grampo">
-            <p id="enviar_foto">Enviar foto</p>
-            <input type="file" id="inputFilePopup" accept="image/*">
-            <img src="${img.src}" id="currentImgPreview" style="max-width: 100px;">
-        </div>
-      `;
-  }
-  
-  conteudoModal += `
-    <button id="submitForm" onclick="submitForm(${input.textContent},${textarea.value},${img.src},${idPai})">Salvar</button>
-  </div>`;
-
-  document.body.insertAdjacentHTML('beforeend', conteudoModal);
-  document.getElementById('btnFecharPopup').onclick = function() {
-      fecharModal();
-  };
-  document.querySelector('.overlayEditar').onclick = function() {
-      fecharModal();
-  };
-};
-
-function fecharModal(conteudoModal) {
-  const overlay = document.querySelector(".overlayEditar");
-  const popup = document.querySelector(".popupEditar");
-  if (overlay.parentNode) {
-    overlay.parentNode.removeChild(overlay);
-  }
-  if (popup.parentNode) {
-    popup.parentNode.removeChild(popup);
-  }
-
-};
-  
-function submitForm(novoInput, novoTextarea, novoCaminhoImg, id) {
-    const formData = {
-        id: id,
-        input: novoInput,
-        textarea: novoTextarea,
-        caminhoImg: novoCaminhoImg
+    document.body.insertAdjacentHTML('beforeend', conteudoModal);
+    document.getElementById('btnFecharPopup').onclick = function() {
+        fecharModal();
     };
-   
-    const jsonData = JSON.stringify(formData);
-
-    fetch('/ParteEditavelController/editar', {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        body: jsonData
-    })
-    .then(response => response.json())
-    .then(data => {
-        console.log('Success:', data);
-    })
-    .catch((error) => {
-        console.error('Error:', error);
-    });
-}
-</script>
+    document.querySelector('.overlayEditar').onclick = function() {
+        fecharModal();
+    };
+  };
+  
+  function fecharModal(conteudoModal) {
+    const overlay = document.querySelector(".overlayEditar");
+    const popup = document.querySelector(".popupEditar");
+    if (overlay.parentNode) {
+      overlay.parentNode.removeChild(overlay);
+    }
+    if (popup.parentNode) {
+      popup.parentNode.removeChild(popup);
+    }
+  
+  };
+    
+  function submitForm(novoInput, novoTextarea, novoCaminhoImg, id) {
+      const formData = {
+          id: id,
+          input: novoInput,
+          textarea: novoTextarea,
+          caminhoImg: novoCaminhoImg
+      };
+     
+      const jsonData = JSON.stringify(formData);
+  
+      fetch('/ParteEditavelController/editar', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+          },
+          body: jsonData
+      })
+      .then(response => response.json())
+      .then(data => {
+          console.log('Success:', data);
+      })
+      .catch((error) => {
+          console.error('Error:', error);
+      });
+  }
+  </script>
+<?php endif; ?>
 
 <style>
 /* the parent has to have position: relative; */

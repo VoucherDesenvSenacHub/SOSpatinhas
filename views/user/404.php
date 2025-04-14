ad410a475200f495aa8e55b1f04d4fc6433e71f0
Lorena-Pinheiro <107314914+Lorena-Pinheiro@users.noreply.github.com>
2025-04-14 12:00:37 -0300

    #327 - Feat / Criação da pag

---
+++ views/user/404.php
@@ -0,0 +1,14 @@
+<?php
+    $cssLink  = '../../public/css/listaEvento.css';
+    include('../../public/componentes/default/topHTML.php');
+?>
+
+<h1>404</h1>
+<p>Oh não! Parece que alguém levou embora a página</p>
+
+<img src="" alt="">
+
+
+<?php
+    include('../../public/componentes/default/bottomHTML.php');
+?>

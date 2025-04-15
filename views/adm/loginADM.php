370d27b8fb427eace26302e4f9e57bd1eb17a08d
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2025-04-15 12:14:39 -0300

    #Testing - Fix / 50%

--- views/adm/loginADM.php
+++ views/adm/loginADM.php
@@ -1,5 +1,5 @@
 <?php
-    session_start();
+  session_start();
 
     if ($_SERVER['REQUEST_METHOD'] == 'POST')
     {
@@ -21,12 +21,12 @@
             {
                 $_SESSION['id'] = "Admin";
                 $_SESSION['taLogado'] = true;
-                header('Location: ../../editarPaginaInicio.php');
+                header('Location: ../views/compartilhada/editarPaginaInicio.php');
                 exit();
             }
         }
     }
-?>
+?> 
 
 <!DOCTYPE html>
 <html lang="pt-BR">
@@ -35,33 +35,27 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="../../public/css/loginADM.css">
+    <link rel="stylesheet" href="../../public/css/default.css">
 </head>
 
 <body>
-    <?php include('../../public/componentes/navbar.php')?> 
-    
-    <section class="corpo-container">
-        <section class="secao-login">
-            <div class="login">
-                <h2>Login</h2>
-                <form method="POST" id="frmLoginADM">
-                    <input type="text" placeholder="ID" name="id">
-                    <input type="password" placeholder="Senha" name="password">
-                    <?php
-                        $idBtn = "btnEntrarAdm";
-                        $funcaoClick = "submitComValidacao('frmLoginADM')";
-                        $funcaoLoad = "mudarTamanho('btnEntrarAdm', '188px', '50px', '20px' )";
-                        $titulo = "Entrar";
-                        include('../../public/componentes/componenteButton.php');
-                    ?>
-                </form>
-            </div>
-        </section>
+    <?php include('../../public/componentes/navbar.php')?>  
+    <section class="secaoLogin">
+        <div class="login">
+            <h2>Login</h2>
+            <form method="POST" id="frmLoginADM">
+                <input type="text" placeholder="ID" name="id">
+                <input type="password" placeholder="Senha" name="password">
+                <?php
+                    $idBtn = "btnEntrarAdm";
+                    $funcaoClick = "submitComValidacao('frmLoginADM')";
+                    $funcaoLoad = "mudarTamanho('btnEntrarAdm', '188px', '50px', '20px' )";
+                    $titulo = "Entrar";
+                    include('../../public/componentes/componenteButton.php');
+                ?>
+            </form>
+        </div>
     </section>
-
-    <?php
-        include('../../public/componentes/default/bottomHTML.php');
-    ?>
-
-    </body>
+    <?php include('../../public/componentes/footer.php'); ?>
+</body>
 </html>

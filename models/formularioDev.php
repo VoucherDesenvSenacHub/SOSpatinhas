e150e20025b51cf16eabe5017f61cf19836d0017
Lorena-Pinheiro <loryfpinheiro@gmail.com>
2025-04-22 00:38:05 -0300

    #notask - Feat / frm dinamico feito, só falta fazer a lógica de colocar colunas e fazer um cleanup

--- models/formularioDev.php
+++ models/formularioDev.php
@@ -1,9 +1,17 @@
 <?php
-require_once("../config/connect.php");
+require_once '../../config/connect.php';
+
 class FormularioDev extends Connect{
-    public function pegaCampos($frmID){
-        $stmt = $this->connection->prepare("SELECT * FROM $this->table WHERE ID_FORM = ?");
-        $stmt->execute($frmID);
+    public function pegaCampos($formName) {
+        $stmt = $this->connection->prepare("SELECT * FROM FORMULARIO_CRIAR_EDITAR WHERE FORMULARIO = ? ORDER BY ORDEM ASC");
+        $stmt->execute([$formName]);
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
     }
 
+    public function pegaCamposComResultado($formName, $id) {
+        $columnName = "ID_" . $formName;
+        $stmt = $this->connection->prepare("SELECT * FROM `$formName` WHERE `$columnName` = ?");
+        $stmt->execute([$id]);
+        return $stmt->fetch(PDO::FETCH_ASSOC);
+    }
 }
--- views/adm/frmCadastarEditar.php
+++ views/adm/frmCadastarEditar.php
@@ -1,19 +1,42 @@
 <?php
+    require_once '../../models/formularioDev.php';
     $cssLink  = '../../public/css/cadastrarAdocao.css';
     include('../../public/componentes/default/topHTML.php');
     $form = $_GET['form'] ?? null;
-    $acao = $_GET['acao'] ?? 'C';
+    $acao = 'E';
     $id = $_GET['id'] ?? null;
+    $model = new FormularioDev();
+    $fields = $model->pegaCampos("ANIMAL");
+    $formData = $model->pegaCamposComResultado("ANIMAL", 1);
 ?>
 
 <form action="<?= $form ?>Controller.php" method="POST" enctype="multipart/form-data" id="frmCadastroAdocao">
-    <input type="hidden" name="form" value="<?= htmlspecialchars($form) ?>">
-    <input type="hidden" name="action" value="<?= htmlspecialchars($action) ?>">
-</form>
+    <input type="hidden" name="acao" value="<?= htmlspecialchars($acao) ?>">
 
-<script>
-   
-</script>
+    <?php foreach ($fields as $field): ?>
+        <?php switch ($field['TIPO']):
+            case 'imgUpload':
+                include('../../public/componentes/imgUpload.php');
+                break;
+            case 'perfilUpload':
+                //fazer um componente do imgUpload pra ft de perfil
+                break;
+            default: 
+            ?>
+                <input
+                    type="<?= $field['TIPO'] ?>"
+                    name="<?= $field['NAME'] ?>"
+                    id="<?= $field['NAME'] ?>"
+                    placeholder="<?= $field['PLACEHOLDER'] ?>"
+                    <?= ($acao === 'E') ? 'value="' . htmlspecialchars($formData[$field['NAME']] ?? '') . '"' : ''; ?>
+                    <?= $field['OBRIGATORIO'] ? 'required' : '' ?>
+                >
+                <?php break; ?>
+        <?php endswitch; ?>
+    <?php endforeach; ?>
+
+    <button type="submit">Salvar</button>
+</form>
 
 <?php
     include('../../public/componentes/default/bottomHTML.php');

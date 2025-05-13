2a75e45c216252752de282426eb5dddd1eeaaf65
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2025-05-13 12:07:40 -0300

    #347 - Feature / implementada condição e ForEach

--- app/views/adm/listas.php
+++ app/views/adm/listas.php
@@ -1,7 +1,13 @@
 <?php
   $cssLink  = '../../public/css/listaMembro.css';
   include('../../componentes/default/topHTML.php');
+  include('../../componentes/cardListas.php'); 
+  include('../../controller/ListaDevController.php');
+
   $tipo = $_GET['tipo'] ?? null;
+
+  $controller = new ListaDevController();
+  $dados = $controller->pegarDados($tipo);
 ?>
 
 <h1>Lista de <?= htmlspecialchars($tipo) ?></h1>
@@ -13,7 +19,23 @@
 
 <div class="lista" id="listaMembro">
     <?php
-        
+        if ($tipo === 'formulario') {
+            foreach ($dados as $formulario) {
+                gerarCardFormulario(
+                    $formulario['tituloFrm'],
+                    $formulario['imagemAnimal'],
+                    $formulario['nomeAnimal'],
+                    $formulario['imagemUser'],
+                    $formulario['nomeUser']
+                );
+            }
+        } elseif ($tipo === 'membro') {
+            foreach ($dados as $membro) {
+                gerarCard($membro['nome'], $membro['imagem']);
+            }
+        } else {
+            echo "<p>Tipo inválido ou nenhum tipo especificado na URL.</p>";
+        }
     ?>
     <div class="btnAddConteiner">
         <a href="cadastrarMembro.php"><button class="btnAdd">+</button></a>

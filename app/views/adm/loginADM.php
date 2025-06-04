95f738010b0855962105a6a4b633e35f6b390824
KauaOSantos <kauaoliveira1506gaf@gmail.com>
2025-06-04 12:03:35 -0300

    arrumado header, footer e falta finalizar o style de senha

--- app/views/adm/loginADM.php
+++ app/views/adm/loginADM.php
@@ -4,28 +4,24 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/loginADM.css">
-    <link rel="stylesheet" href="public/css/default.css">
+    <link rel="stylesheet" href="../public/css/loginADM.css">
+    <link rel="stylesheet" href="../public/css/default.css">
 </head>
 
 <body>
-    <?php include('app/componentes/navbar.php')?>  
+    <?php include('app/componentes/default/topHTML.php')?>  
     <section class="secaoLogin">
         <div class="login">
             <h2>Login</h2>
             <form action="login" method="POST" id="frmLoginADM">
-                <input type="text" placeholder="Email:" name="EMAIL" required>
-
-
-
-                <input type="password" id="senha" name="SENHA" required placeholder="Senha:"> <!-- do meu form -->
-                <!-- <input type="password" placeholder="Senha:" name="SENHA" id="inputSenha">  já estava aqui -->
-                <button type="button" id="btnIconOlho">
-                    <img id="imgIconOlho" src="../public/images/icons/iconOlhoAberto.png" alt="Mostrar senha">
-                </button>
-            </div>
-
-                <input type="password" placeholder="Senha:" name="SENHA" id="password">
+                <input type="email" placeholder="Email:" name="EMAIL" required>
+                <div class="containerSenha">
+                    <input type="password" id="inputSenha" placeholder="Senha:" name="SENHA" required>  
+                    <button type="button" class="btnIconOlho">
+                        <img id="imgIconOlho" src="../public/images/icons/iconOlhoAberto.png" alt="Mostrar senha">
+                    </button>
+                </div>
+        </div>
                 <?php if (isset($_SESSION['erroLogin'])): ?>
                         <p style="color:red;"><?php echo htmlspecialchars($_SESSION['erroLogin']); ?></p>
                         <?php unset($_SESSION['erroLogin']); ?>
@@ -41,22 +37,21 @@
         </div>
     </section>
     <script>
-         document.getElementById("btnIconOlho").addEventListener("click", function() {
-            let inputSenha = document.getElementById("senha");
-            let imgIconOlho = document.getElementById("imgIconOlho");
+        document.querySelector(".btnIconOlho").addEventListener("click", function () {
+            const inputSenha = document.getElementById("inputSenha");
+            const imgIconOlho = this.querySelector("img");
 
             if (inputSenha.type === "password") {
                 inputSenha.type = "text";
-                imgIconOlho.src = "../public/images/icons/iconOlhoFechado.png"; 
+                imgIconOlho.src = "../public/images/icons/iconOlhoFechado.png";
                 imgIconOlho.alt = "Esconder senha";
-            } 
-            else {
+            } else {
                 inputSenha.type = "password";
-                imgIconOlho.src = "../public/images/icons/iconOlhoAberto.png"; 
+                imgIconOlho.src = "../public/images/icons/iconOlhoAberto.png";
                 imgIconOlho.alt = "Mostrar senha";
             }
         });
     </script>
-    <?php include('app/componentes/footer.php'); ?>
+    <?php include('app/componentes/default/bottomHTML.php'); ?>
 </body>
 </html>

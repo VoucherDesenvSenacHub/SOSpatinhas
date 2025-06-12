bb8284c0f00220bbefe9ead326edc3ae294de635
gustav0Oliveira <gusoliveira2006@gmail.com>
2025-06-12 11:01:29 -0400

    Arrumando redirecionamento que foi esquecido

--- app/views/adm/loginADM.php
+++ app/views/adm/loginADM.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="public/css/loginADM.css">
-    <link rel="stylesheet" href="public/css/default.css">
+    <link rel="stylesheet" href="/sospatinhas/public/css/loginADM.css">
+    <link rel="stylesheet" href="/sospatinhas/public/css/default.css">
 </head>
 
 <body>

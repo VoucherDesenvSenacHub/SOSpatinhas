670a55ad6d20025e49ecb3d12b3ecd9bac074587
Lorena-Pinheiro <loryfpinheiro@gmail.com>
2025-07-23 03:05:16 -0300

    pag Evento pronta

--- app/componentes/eventosCard.php
+++ app/componentes/eventosCard.php
@@ -1,5 +1,5 @@
 <div class="eventosCard" onclick="window.location.href='<?= '/sospatinhas/eventos/detalhesevento/'.$evento['ID_EVENTO']; ?>'">
-    <img src="/sospatinhas/public/images/eventosimg/eventos4.png" alt="Imagem do evento">
+    <img src="/sospatinhas/public/images/default/evento2.png" alt="Imagem do evento">
     <div class="cardInfo">
       <h3><?= $evento["TITULO"];?></h3>
       <p><?= $evento["DESCRICAO"];?></p>
--- app/views/user/eventos.php
+++ app/views/user/eventos.php
@@ -4,47 +4,81 @@
 ?>
 
 <section class="content">
-    <div class="row">
+    <!-- <div class="row">
         <h1 class="titulo">Arrecadações</h1>
         <div id="arrecadacaoCarrosel">
             <?php
-                $cardComponents1 = array();
+                // $cardComponents1 = array();
 
-                for ($i = 0; $i < 5; $i++) {    
-                    ob_start(); 
-                    include('app/componentes/arrecadacaoCard.php'); 
-                    $cardComponents1[] = ob_get_clean();
-                }
+                // for ($i = 0; $i < 5; $i++) {    
+                //     ob_start(); 
+                //     include('app/componentes/arrecadacaoCard.php'); 
+                //     $cardComponents1[] = ob_get_clean();
+                // }
 
-                $cardComponents = $cardComponents1;
-                $carouselId = 'carousel1';
-                @include('app/componentes/carossel.php');
+                // $cardComponents = $cardComponents1;
+                // $carouselId = 'carousel1';
+                // @include('app/componentes/carossel.php');
             ?>
         </div> 
-    </div>
+    </div> -->
         
     <div class="row">
-        <h1 class="titulo">Eventos</h1>
+        <h1 class="titulo">Eventos Futuros</h1>
         <div id="eventosCarrosel">
             <?php
-            require_once('app/models/EventoModel.php');
-            $eventoModel = new EventoModel();
-
-            $acao = ['ACAO' => "R"];
-            $jsonData = json_encode($acao);
-            $resultado = $eventoModel->CRUD($jsonData);
-
-            $cardComponents2 = array();
-            foreach ($resultado as $evento) {
-            ob_start();
-            include('app/componentes/eventosCard.php');
-            $cardComponents2[] = ob_get_clean();
-            }
-            
+            try{
+                require_once('app/models/EventoModel.php');
+                $eventoModel = new EventoModel();
     
-            $cardComponents = $cardComponents2;
-            $carouselId = 'carousel2';
-            @include('app/componentes/carossel.php');
+                $acao = ['ACAO' => "R"];
+                $jsonData = json_encode($acao);
+                $resultado = $eventoModel->CRUD($jsonData);
+    
+                $cardComponents2 = array();
+                foreach ($resultado as $evento) {
+                ob_start();
+                include('app/componentes/eventosCard.php');
+                $cardComponents2[] = ob_get_clean();
+                }
+                
+        
+                $cardComponents = $cardComponents2;
+                $carouselId = 'carousel2';
+                @include('app/componentes/carossel.php');
+            }catch (Exception $e) {
+                setModal('erro', 'Erro encontrado!', $e->getMessage());
+            }
+            ?>
+        </div>
+    </div>
+
+    <div class="row">
+        <h1 class="titulo">Eventos Passados</h1>
+        <div id="eventosCarrosel">
+            <?php
+            try{
+                require_once('app/models/EventoModel.php');
+                $eventoModel = new EventoModel();
+    
+                $acao = ['ACAO' => "R"];
+                $jsonData = json_encode($acao);
+                $resultado = $eventoModel->CRUD($jsonData);
+    
+                $cardComponents2 = array();
+                foreach ($resultado as $evento) {
+                ob_start();
+                include('app/componentes/eventosCard.php');
+                $cardComponents2[] = ob_get_clean();
+                }
+                
+        
+                $cardComponents = $cardComponents2;
+                $carouselId = 'carousel2';
+                @include('app/componentes/carossel.php');
+            }catch (Exception $e) {
+                setModal('erro', 'Erro encontrado!', $e->getMessage());
+            }
             ?>
         </div>
     </div>
